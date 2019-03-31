f2db6fc38940d0cdf7daa0d2d0c5990b32beaf1f
Bram Krick <bram.krick@student.hogent.be>
2019-03-31 03:05:40 +0200

    Afwerken startpagina
    Startpagina stuurt door naar de teller.
    Constraint layout in orde gezet en alles goed geplaatst.

--- app/src/main/java/com/example/manillenandroid/MainActivity.java
+++ app/src/main/java/com/example/manillenandroid/MainActivity.java
@@ -1,55 +1,40 @@
 package com.example.manillenandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView scoreTeam1;
-    private TextView scoreTeam2;
-    private EditText scoreDezeRonde;
-    private Button team1;
-    private Button team2;
-    private int totaalTeam1;
-    private int totaalTeam2;
+    public static String naamTeam1;
+    public static String naamTeam2;
+    private Button submit;
+    private EditText et_naamTeam1;
+    private EditText et_naamTeam2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_startscherm);
 
-        scoreDezeRonde = (EditText) findViewById(R.id.etScoreRonde);
-        team1 = (Button) findViewById(R.id.btnTeam1);
-        team2 = (Button) findViewById(R.id.btnTeam2);
-        scoreTeam1 = (TextView) findViewById(R.id.tvTeam1);
-        scoreTeam2 = (TextView) findViewById(R.id.tvTeam2);
-
-        team1.setOnClickListener(new View.OnClickListener() {
+        submit = findViewById(R.id.btn_submitNaam);
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int scoreRonde = Integer.parseInt(scoreDezeRonde.getText().toString());
-                totaalTeam1 = totaalTeam1 + scoreRonde;
-                scoreTeam1.setText("team1: " + String.valueOf(totaalTeam1));
-
+                et_naamTeam1 = findViewById(R.id.et_naamTeam1);
+                et_naamTeam2 = findViewById(R.id.et_naamTeam2);
+                naamTeam1 = et_naamTeam1.getText().toString();
+                naamTeam2 = et_naamTeam2.getText().toString();
+                openOpteller();
             }
-
-
         });
-        team2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int scoreRonde = Integer.parseInt(scoreDezeRonde.getText().toString());
-                totaalTeam2 = totaalTeam2 + scoreRonde;
-                scoreTeam2.setText("team1: " + String.valueOf(totaalTeam2));
+    }
 
-            }
-
-
-        });
+    private void openOpteller() {
+        Intent openOptellerKlasse = new Intent(this, ScoreOptellen.class);
+        startActivity(openOptellerKlasse);
     }
 }
